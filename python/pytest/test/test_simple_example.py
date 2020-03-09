ff0a1a254c440b1f45ce227d07f64352ea4cddce
Wilsonsmi <33430924+Wilsonsmi@users.noreply.github.com>
2020-03-09 12:42:19 +0530

    Update test_simple_example.py

--- python/pytest/test/test_simple_example.py
+++ python/pytest/test/test_simple_example.py
@@ -1,26 +1,28 @@
-#!/usr/bin/env python
-"""Simple Selenium WebDriver test that uses TestChameleon Selenium"""
+# #!/usr/bin/env python
+# """Simple Selenium WebDriver test that uses TestChameleon Selenium"""
 
-# import unittest
-# from selenium import webdriver
-# from selenium.webdriver.common.keys import Keys
-# from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+# # import unittest
+# # from selenium import webdriver
+# # from selenium.webdriver.common.keys import Keys
+# # from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
 
 class PythonPytestSimpleTest():
-    """A simple Selenium example test with Python and pytest."""
+#     """A simple Selenium example test with Python and pytest."""
 
-    # def setUp(self):
-    #     """Setup before test."""
-    #     # capas = DesiredCapabilities.FIREFOX
-    #     # self.driver = webdriver.Remote(desired_capabilities=capas)
+#     # def setUp(self):
+#     #     """Setup before test."""
+#     #     # capas = DesiredCapabilities.FIREFOX
+#     #     # self.driver = webdriver.Remote(desired_capabilities=capas)
 
-    def test_title(self):
+    def test_title():
+        print "wilsonn"
 #         """Test the title of our demo page."""
 #         self.driver.get("https://demo.testchameleon.com/")
 #         assert "Gentellela Alela!" in self.driver.title
 
-    def tearDown(self):
+    def tearDown():
+        print "smth"
 #         """Cleanup after test."""
 #         self.driver.quit()
 
